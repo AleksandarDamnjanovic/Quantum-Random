c1794fa5f9c3ad0ecf8062527d086fce2b626d70
AleksandarDamnjanovic <aor.aleksandar@gmail.com>
2021-11-19 21:47:33 +0100

    Updated

---
+++ QuantomRand.R
@@ -0,0 +1,49 @@
+#******************************************
+#Title: Quantum Random
+#Author: Aleksandar Damnjanovic
+#Date: 19.11.2021
+#******************************************
+
+library("httr") 
+library("jsonlite")
+
+nextOne<-function(){
+  addr<-GET("https://qrng.anu.edu.au/API/jsonI.php?length=1&type=hex16&size=4")
+  page<-content(addr,"text")
+  json<-fromJSON(page, flatten = TRUE)
+  json<-as.data.frame(json)
+  val<-json$data
+  val<-paste0('0x', val)
+  val<-as.numeric(val)
+  val<-sprintf("%.0f", val)
+  Sys.sleep(.5)
+  return(val)
+}
+
+getRand<-function(minv, maxv){
+  bigVal<-nextOne()
+  bigVal<-as.numeric(bigVal)
+  minv<-as.numeric(minv)
+  maxv<-as.numeric(maxv)
+  
+  if(minv<0 || maxv>.Machine$integer.max)
+    print(paste('min must be bigger than or0;', 'max must be less than', .Machine$integer.max))
+
+  ret<-0
+  con<-(bigVal>maxv)
+  if(maxv!=.Machine$integer.max)
+    if(con)
+      ret<-bigVal %% maxv
+  
+  if(ret<minv && minv!=0)
+    ret= minv + (bigVal %% (maxv-minv))
+  
+  ret<-sprintf("%.0f", ret)
+  return(ret)  
+}
+
+
+for(i in c(0:15)){
+  r<-getRand(0,100)
+  print(r)
+}
